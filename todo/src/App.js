0fd91c63f35b3475749ec8b771277eca13db98ce
Eduardo <edalvarez2015@gmail.com>
2021-02-19 12:02:09 -0600

    continuing to work on MDN todo list

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,19 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
 import logo from "./logo.svg";
 import "./App.css";
 import Todo from "./Todo";
-import Form from "../components/Form";
-import FilterButton from "../components/FilterButton";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App(props) {
-  const taskList = props.tasks.map((task) => (
+  const [tasks, setTask] = useState(props.tasks);
+  function addTask(task) {
+    const newTask = { id: `todo-${nanoid()}`, name: task, completed: false };
+    setTask([...tasks, newTask]);
+  }
+  const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
       key={task.id}
@@ -13,13 +21,16 @@ function App(props) {
       completed={task.completed}
     />
   ));
-
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <FilterButton />
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <FilterButton />
+      <FilterButton />
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
